64acdd12a097b7f64c51ec1cd6550acc9595bdc0
Jens Broerken <70336876+jbroerken@users.noreply.github.com>
2021-12-22 20:00:21 +0100

    Remove libmrhpcdev support

--- src/Main.cpp
+++ src/Main.cpp
@@ -27,7 +27,6 @@
 // External
 #include <libmrh/MRH_AppLoop.h>
 #include <libmrhab.h>
-//#include <libmrhpcdev.h>
 
 // Project
 #include "./Module/RepeatAfterMe/RepeatAfterMe.h"
@@ -160,124 +159,6 @@ extern "C"
             delete p_Context;
         }
     }
-    
-    //*************************************************************************************
-    // PC Dev
-    //*************************************************************************************
-    
-#ifdef MRH_PC_DEV_LIB_IN_USE
-    
-    namespace
-    {
-        // @NOTE: Value runs from -2 to 0.
-        //        2 SAY outputs for each input, ASK -> LISTEN -> REPEAT -> ASK -> ...
-        //        Start at -1, first run only has 1 output before listen!
-        int i_CanAnswer = -1;
-    }
-    
-    MRH_Event* CallbackListen(const MRH_Event* p_Event)
-    {
-        static MRH_Uint32 u32_ID = 0;
-        ++u32_ID;
-        
-        return MRH_L_STRING_S(MRH_L_STRING_S::END, u32_ID, 0, "Hello!").Build();
-    }
-    
-    MRH_Event* CallbackSay(const MRH_Event* p_Event)
-    {
-        static MRH_SpeechString c_String(0);
-        MRH_S_STRING_U c_Event(p_Event);
-        bool b_End = c_Event.GetType() == MRH_S_STRING_U::END ? true : false;
-        
-        if (c_Event.GetID() == c_String.GetID())
-        {
-            c_String.Add(c_Event.GetString(),
-                         c_Event.GetPart(),
-                         b_End);
-        }
-        else
-        {
-            c_String.Reset(c_Event.GetString(),
-                           c_Event.GetID(),
-                           c_Event.GetPart(),
-                           b_End);
-        }
-        
-        if (c_String.GetState() == MRH_SpeechString::COMPLETE)
-        {
-            std::cout << c_String.GetString() << std::endl;
-            c_String.Reset(0);
-            
-            ++i_CanAnswer;
-        }
-        
-        return NULL;
-    }
-    
-    MRH_Event* CallbackLaunch(const MRH_Event* p_Event)
-    {
-        MRH_A_LAUNCH_SOA_U c_Event(p_Event);
-        return MRH_A_LAUNCH_SOA_S(c_Event.GetPackagePath(),
-                                  c_Event.GetLaunchInput(),
-                                  c_Event.GetLaunchCommandID()).Build();
-    }
-    
-    MRH_Event* CallbackServiceAvail(const MRH_Event* p_Event)
-    {
-        switch (p_Event->u32_Type)
-        {
-            case MRH_EVENT_LISTEN_AVAIL_U:
-                return MRH_L_AVAIL_S(true, 0, 0, 1).Build();
-            case MRH_EVENT_SAY_AVAIL_U:
-                return MRH_S_AVAIL_S(true, 0, 0, 1).Build();
-                
-            default:
-                return NULL;
-        }
-    }
-    
-    int MRH_PCD_Setup(void)
-    {
-        // Set app environment
-        MRH_PCD_EnvironmentType(MRH_PCDEV_ENV_APPLICATION);
-        MRH_PCD_FSRoot("/Users/Jens/Documents/GitHub/MRH/App/de.mrh.mirrorspeech/pkg/FSRoot/");
-        
-        // Add event callbacks
-        MRH_PCD_EventSending(10, 100);
-        MRH_PCD_EventCallback(&CallbackListen, MRH_EVENT_LISTEN_STRING_S);
-        MRH_PCD_EventCallback(&CallbackSay, MRH_EVENT_SAY_STRING_U);
-        MRH_PCD_EventCallback(&CallbackServiceAvail, MRH_EVENT_LISTEN_AVAIL_U);
-        MRH_PCD_EventCallback(&CallbackServiceAvail, MRH_EVENT_SAY_AVAIL_U);
-        
-        // Set permissions
-        MRH_PCD_PermissionsListen(MRH_PCD_PERMISSION_LISTEN_LISTEN);
-        MRH_PCD_PermissionsSay(MRH_PCD_PERMISSION_SAY_SAY);
-        
-        // Set app launch input
-        MRH_PCD_AppLaunchParam("", 0, 0);
-        
-        // Add Functions
-        MRH_PCD_FunctionAppInit(&MRH_Init);
-        MRH_PCD_FunctionAppRecieveEvent(&MRH_RecieveEvent);
-        MRH_PCD_FunctionSendEvent(&MRH_SendEvent);
-        MRH_PCD_FunctionAppCanExit(&MRH_CanExit);
-        MRH_PCD_FunctionExit(&MRH_Exit);
-        
-        return 0;
-    }
-    
-    int MRH_PCD_Update(void)
-    {
-        if (i_CanAnswer == 0)
-        {
-            i_CanAnswer = -2;
-            MRH_PCD_TriggerCallback(NULL, MRH_EVENT_LISTEN_STRING_S);
-        }
-        
-        return 0;
-    }
-    
-#endif
 
 #ifdef __cplusplus
 }
